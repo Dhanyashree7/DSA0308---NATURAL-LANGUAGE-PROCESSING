606ecc5f5eda15315c61fb3d3eaf76015ad5fa6c
Dhanyashree7 <abdhanyashree@gmail.com>
2026-08-06 11:05:01 +0530

    Updated Q1.py
    Added regex to extract email and phone number from text.

--- LAB EXP/Q1.py
+++ LAB EXP/Q1.py
@@ -1,1 +1,10 @@
-
+import re
+text = "My email is student123@gmail.com and my phone number is 9876543210."
+email = re.search(r"\S+@\S+", text)
+phone = re.search(r"\d{10}", text)
+print("Original Text:")
+print(text)
+if email:
+    print("\nEmail Found:", email.group())
+if phone:
+    print("Phone Number Found:", phone.group())

689da59a4d0af64c04babd9a8a3a3d5f30ee01e7
Dhanyashree7 <abdhanyashree@gmail.com>
2026-08-06 11:38:18 +0530

    Updated Q6.py

--- LAB EXP/Q6.py
+++ LAB EXP/Q6.py
@@ -1,1 +1,18 @@
+text = "I love natural language processing and I love python"
+words = text.split()
+bigrams = []
 
+for i in range(len(words) - 1):
+    bigrams.append((words[i], words[i + 1]))
+
+print("Bigrams:")
+print(bigrams)
+word = "I"
+
+print("\nGenerated Text:")
+print(word, end=" ")
+
+for first, second in bigrams:
+    if first == word:
+        print(second, end=" ")
+        word = second

a271e1aa1a19b5976404f2fe48d2c0146948f507
Dhanyashree7 <abdhanyashree@gmail.com>
2026-08-06 11:31:39 +0530

    Updated Q4.py
    Added a function to convert singular nouns to plural forms based on specific rules.

--- LAB EXP/Q4.py
+++ LAB EXP/Q4.py
@@ -1,1 +1,16 @@
+def plural(noun):
 
+    if noun.endswith("y"):
+        return noun[:-1] + "ies"
+
+    elif noun.endswith(("s", "x", "z", "ch", "sh")):
+        return noun + "es"
+
+    else:
+        return noun + "s"
+nouns = ["cat", "dog", "box", "bus", "baby", "watch"]
+
+print("Singular\tPlural")
+
+for word in nouns:
+    print(word, "\t\t", plural(word))

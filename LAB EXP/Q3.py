34e39c693ea80934a8b845afefd99096e9b91698
Dhanyashree7 <abdhanyashree@gmail.com>
2026-08-06 11:21:31 +0530

    Updated Q3.py

--- LAB EXP/Q3.py
+++ LAB EXP/Q3.py
@@ -1,1 +1,12 @@
+import nltk
+from nltk.stem import PorterStemmer
+nltk.download('punkt')
+ps = PorterStemmer()
+words = ["connected", "connecting", "connection",
+         "playing", "played", "player"]
 
+print("Word\t\tStem")
+
+for word in words:
+    stem = ps.stem(word)
+    print(f"{word}\t\t{stem}")

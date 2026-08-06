0f8da5f1a260fda6a7b7da0556d0717eebbe012b
Dhanyashree7 <abdhanyashree@gmail.com>
2026-08-06 11:42:23 +0530

    Updated Q9.PY
    Added a simple part-of-speech tagging functionality to classify words in a sentence.

--- LAB EXP/Q9.py
+++ LAB EXP/Q9.py
@@ -1,1 +1,26 @@
+import re
 
+sentence = input("Enter a sentence: ")
+
+words = sentence.split()
+
+print("\nPOS Tags")
+
+for word in words:
+
+    if re.fullmatch(r"The|A|An", word):
+        tag = "DT"
+
+    elif re.fullmatch(r".*ing", word):
+        tag = "VBG"
+
+    elif re.fullmatch(r".*ed", word):
+        tag = "VBD"
+
+    elif re.fullmatch(r".*ly", word):
+        tag = "RB"
+
+    else:
+        tag = "NN"
+
+    print(word,"->",tag)

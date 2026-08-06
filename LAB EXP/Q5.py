e6fbf4cb482a7d65df521fd6d564d4bbcbe43454
Dhanyashree7 <abdhanyashree@gmail.com>
2026-08-06 11:36:24 +0530

    Updated Q5.py
    Added NLTK library for stemming words and implemented a simple stemmer using PorterStemmer.

--- LAB EXP/Q5.py
+++ LAB EXP/Q5.py
@@ -1,1 +1,13 @@
+import nltk
+from nltk.stem import PorterStemmer
+nltk.download('punkt')
+ps = PorterStemmer()
+words = ["connected", "connecting", "connection",
+         "playing", "played", "player",
+         "studies", "studying", "studied"]
 
+print("Original Word\tStemmed Word")
+
+for word in words:
+    stem = ps.stem(word)
+    print(f"{word}\t\t{stem}")

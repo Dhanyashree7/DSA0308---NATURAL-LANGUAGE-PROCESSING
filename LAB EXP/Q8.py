9b0a97355bb71e878b3b07a95e80141f7228c0ea
Dhanyashree7 <abdhanyashree@gmail.com>
2026-08-06 11:41:54 +0530

    Updated Q8.py
    Added a simple part-of-speech tagging functionality that uses a predefined training dictionary to tag words in a user-input sentence.

--- LAB EXP/Q8.py
+++ LAB EXP/Q8.py
@@ -1,1 +1,22 @@
+training = {
+    "The":"DT",
+    "cat":"NN",
+    "drinks":"VBZ",
+    "milk":"NN",
+    "boy":"NN",
+    "eats":"VBZ"
+}
 
+sentence = input("Enter a sentence: ")
+
+words = sentence.split()
+
+print("\nPOS Tags")
+
+for word in words:
+
+    if word in training:
+        print(word,"->",training[word])
+
+    else:
+        print(word,"->","NN")

dc7e3b16ea7c754f4418274fd4a035e0fa7836c7
Dhanyashree7 <abdhanyashree@gmail.com>
2026-08-06 11:40:03 +0530

    Updated Q7.py

--- LAB EXP/Q7.py
+++ LAB EXP/Q7.py
@@ -1,1 +1,14 @@
+import nltk
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
 
+text = "The cat drinks milk"
+
+words = nltk.word_tokenize(text)
+
+tags = nltk.pos_tag(words)
+
+print("Words with POS Tags:")
+
+for word, tag in tags:
+    print(word, "->", tag)

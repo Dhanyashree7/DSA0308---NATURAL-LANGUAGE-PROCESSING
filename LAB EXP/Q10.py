945260ad74fe71ad0774832945e987d6b025925d
Dhanyashree7 <abdhanyashree@gmail.com>
2026-08-06 11:42:50 +0530

    Updated Q10.py

--- LAB EXP/Q10.py
+++ LAB EXP/Q10.py
@@ -1,1 +1,23 @@
+sentence = input("Enter a sentence: ")
 
+words = sentence.split()
+
+print("\nPOS Tags")
+
+for word in words:
+
+    tag = "NN"
+
+    if word.lower() in ["the","a","an"]:
+        tag = "DT"
+
+    if word.endswith("ing"):
+        tag = "VBG"
+
+    if word.endswith("ed"):
+        tag = "VBD"
+
+    if word.endswith("ly"):
+        tag = "RB"
+
+    print(word,"->",tag)

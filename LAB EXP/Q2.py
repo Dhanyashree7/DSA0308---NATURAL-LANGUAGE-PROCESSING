a5d65f82c5b258d6350e5f83ddea61af717fa2e8
Dhanyashree7 <abdhanyashree@gmail.com>
2026-08-06 11:18:48 +0530

    Updated Q2.py
    Implement finite state automaton to accept strings ending with 'ab'.

--- LAB EXP/Q2.py
+++ LAB EXP/Q2.py
@@ -1,1 +1,32 @@
+def finite_state_automaton(string):
+    state = 0
 
+    for ch in string:
+        if state == 0:
+            if ch == 'a':
+                state = 1
+            else:
+                state = 0
+
+        elif state == 1:
+            if ch == 'b':
+                state = 2
+            elif ch == 'a':
+                state = 1
+            else:
+                state = 0
+
+        elif state == 2:
+            if ch == 'a':
+                state = 1
+            else:
+                state = 0
+
+    if state == 2:
+        print(string, "-> Accepted (Ends with 'ab')")
+    else:
+        print(string, "-> Rejected")
+strings = ["ab", "aab", "aaab", "abab", "abc", "baa", "cab"]
+
+for s in strings:
+    finite_state_automaton(s)

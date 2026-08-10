68256ae272e69cb0eadfe243f862e34867c3112e
Dhanyashree7 <abdhanyashree@gmail.com>
2026-08-10 08:33:24 +0530

    Updated Q14.py

--- LAB EXP/Q14.py
+++ LAB EXP/Q14.py
@@ -1,1 +1,14 @@
-
+sentence = input("Enter sentence: ").lower().split()
+singular = ["boy", "girl", "cat"]
+plural = ["boys", "girls", "cats"]
+if len(sentence) == 2:
+    subject = sentence[0]
+    verb = sentence[1]
+    if subject in singular and verb.endswith("s"):
+        print("Agreement Correct")
+    elif subject in plural and not verb.endswith("s"):
+        print("Agreement Correct")
+    else:
+        print("Agreement Incorrect")
+else:
+    print("Invalid sentence")

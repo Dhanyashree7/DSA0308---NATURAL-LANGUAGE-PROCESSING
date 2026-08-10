8def6aba0aea933576bdec014f025797c0872294
Dhanyashree7 <abdhanyashree@gmail.com>
2026-08-10 08:44:44 +0530

    Updated Q17.py

--- LAB EXP/Q17.py
+++ LAB EXP/Q17.py
@@ -1,1 +1,9 @@
-
+import nltk
+from nltk.corpus import wordnet
+nltk.download("wordnet")
+word = input("Enter a word: ")
+synsets = wordnet.synsets(word)
+for syn in synsets:
+    print("Synset:", syn.name())
+    print("Meaning:", syn.definition())
+    print()

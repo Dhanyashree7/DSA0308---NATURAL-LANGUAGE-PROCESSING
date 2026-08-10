24cf1ed282b531b547fabf395cb6e39430447cab
Dhanyashree7 <abdhanyashree@gmail.com>
2026-08-10 08:38:40 +0530

    Updated Q16.py

--- LAB EXP/Q16.py
+++ LAB EXP/Q16.py
@@ -1,1 +1,7 @@
-
+import spacy
+nlp = spacy.load("en_core_web_sm")
+text = "Apple is located in California. Tim Cook is the CEO."
+doc = nlp(text)
+print("Named Entities:")
+for entity in doc.ents:
+    print(entity.text, "->", entity.label_)

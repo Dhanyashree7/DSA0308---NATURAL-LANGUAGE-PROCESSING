e431040cf21f4783a68543becbba01dec8b120ae
Dhanyashree7 <abdhanyashree@gmail.com>
2026-08-10 08:31:16 +0530

    Updated Q13.py
    Added code to print a parse tree for a simple sentence.

--- LAB EXP/Q13.py
+++ LAB EXP/Q13.py
@@ -1,1 +1,8 @@
-
+sentence = ["the", "boy", "eats"]
+print("Parse Tree")
+print("S")
+print("|-- NP")
+print("|   |-- Det :", sentence[0])
+print("|   |-- N   :", sentence[1])
+print("|-- VP")
+print("    |-- V   :", sentence[2])

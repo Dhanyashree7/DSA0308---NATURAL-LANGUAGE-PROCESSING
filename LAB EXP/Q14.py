696a27201edc9ea82cd72171a727fcf654c8f20b
Dhanyashree7 <abdhanyashree@gmail.com>
2026-08-11 09:23:00 +0530

    Updated Q14.py
    Refactored sentence agreement checking logic and added grammar rules.

--- LAB EXP/Q14.py
+++ LAB EXP/Q14.py
@@ -1,14 +1,72 @@
-sentence = input("Enter sentence: ").lower().split()
-singular = ["boy", "girl", "cat"]
-plural = ["boys", "girls", "cats"]
-if len(sentence) == 2:
-    subject = sentence[0]
-    verb = sentence[1]
-    if subject in singular and verb.endswith("s"):
-        print("Agreement Correct")
-    elif subject in plural and not verb.endswith("s"):
-        print("Agreement Correct")
-    else:
-        print("Agreement Incorrect")
+grammar={
+"S":["NP VP"],
+"NP":["Det N"],
+"VP":["V"],
+"Det":["the"],
+"N_singular":["boy","girl","student"],
+"N_plural":["boys","girls","students"],
+"V_singular":["runs","plays","eats"],
+"V_plural":["run","play","eat"]
+}
+def check agreement(sentence):
+words=sentence.lower().spli()
+if len(words)!=3:
+print("Sentence should have 3 words:")
+det,noun,verb=words
+if det!="the":
+print("sentence noi in agreement:")
+if noun in grammar["N_Singular"] and verb in grammar["V_Singular"]:
+print("Sentence in agreement:")
+elif noun in grammer["N_plural"] and verb in grammar["V_plural"]:
+print("Sentence in agreement:")
 else:
-    print("Invalid sentence")
+print("not in agreement:")
+sentences={
+"the boy runs",
+"the girl plays",
+"the boys eat",
+"the girls eat",
+"the boy run",
+"the boys runs"
+}
+for sentence in sentences:
+print(f"{sentence} -{check_agreement(sentence)}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

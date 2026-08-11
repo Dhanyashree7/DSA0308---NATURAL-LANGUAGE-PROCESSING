782f0771fa43490b20ca3c8ab24ce50bd9a83d53
Dhanyashree7 <abdhanyashree@gmail.com>
2026-08-11 09:47:38 +0530

    Updated Q14.py

--- LAB EXP/Q14.py
+++ LAB EXP/Q14.py
@@ -1,72 +1,33 @@
-grammar={
-"S":["NP VP"],
-"NP":["Det N"],
-"VP":["V"],
-"Det":["the"],
-"N_singular":["boy","girl","student"],
-"N_plural":["boys","girls","students"],
-"V_singular":["runs","plays","eats"],
-"V_plural":["run","play","eat"]
+grammar = {
+    "S": ["NP VP"],
+    "NP": ["Det N"],
+    "VP": ["V"],
+    "Det": ["the"],
+    "N_Singular": ["boy", "girl", "student"],
+    "N_Plural": ["boys", "girls", "students"],
+    "V_Singular": ["runs", "plays", "eats"],
+    "V_Plural": ["run", "play", "eat"]
 }
-def check agreement(sentence):
-words=sentence.lower().spli()
-if len(words)!=3:
-print("Sentence should have 3 words:")
-det,noun,verb=words
-if det!="the":
-print("sentence noi in agreement:")
-if noun in grammar["N_Singular"] and verb in grammar["V_Singular"]:
-print("Sentence in agreement:")
-elif noun in grammer["N_plural"] and verb in grammar["V_plural"]:
-print("Sentence in agreement:")
-else:
-print("not in agreement:")
-sentences={
-"the boy runs",
-"the girl plays",
-"the boys eat",
-"the girls eat",
-"the boy run",
-"the boys runs"
+def check_agreement(sentence):
+    words = sentence.lower().split()
+    if len(words) != 3:
+        return "Sentence should have exactly 3 words"
+    determiner, noun, verb = words
+    if determiner != "the":
+        return "Sentence is not agreement"
+    if noun in grammar["N_Singular"] and verb in grammar["V_Singular"]:
+        return "Sentence is agreement"
+    elif noun in grammar["N_Plural"] and verb in grammar["V_Plural"]:
+        return "Sentence is agreement"
+    else:
+        return "Sentence is not agreement"
+sentences = {
+    "the boy runs",
+    "the girl plays",
+    "the boys eat",
+    "the girls eat",
+    "the boy run",
+    "the boys runs"
 }
 for sentence in sentences:
-print(f"{sentence} -{check_agreement(sentence)}")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    print(f"{sentence} -> {check_agreement(sentence)}")

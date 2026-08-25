c3885850a074381f3490b87e828521b857dd5d31
Dhanyashree7 <abdhanyashree@gmail.com>
2026-08-25 10:07:37 +0530

    Updated Q24.py

--- LAB EXP/Q24.py
+++ LAB EXP/Q24.py
@@ -1,1 +1,19 @@
+text = input("Enter dialog: ").lower()
 
+if "?" in text:
+    print("Dialog Act: Question")
+
+elif text.startswith("please"):
+    print("Dialog Act: Request")
+
+elif text.startswith("yes") or text.startswith("okay"):
+    print("Dialog Act: Agreement")
+
+elif "thank" in text:
+    print("Dialog Act: Thanking")
+
+elif text.startswith("hello") or text.startswith("hi"):
+    print("Dialog Act: Greeting")
+
+else:
+    print("Dialog Act: Statement")

f19701f94edd95aa1b1e175e001297c0b6d1e35c
Dhanyashree7 <abdhanyashree@gmail.com>
2026-08-25 10:04:42 +0530

    Updated Q21.py
    Added functionality to extract noun phrases and their meanings using WordNet.

--- LAB EXP/Q21.py
+++ LAB EXP/Q21.py
@@ -1,1 +1,19 @@
+import nltk
+from nltk.corpus import wordnet
 
+nltk.download("wordnet")
+
+sentence = input("Enter sentence: ")
+
+words = sentence.lower().split()
+
+print("\nNoun Phrases and Meanings:")
+
+for word in words:
+
+    synsets = wordnet.synsets(word, pos=wordnet.NOUN)
+
+    if synsets:
+        print("Noun:", word)
+        print("Meaning:", synsets[0].definition())
+        print()

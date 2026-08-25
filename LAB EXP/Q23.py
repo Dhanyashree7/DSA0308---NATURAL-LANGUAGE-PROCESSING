5845dbcf07bbd61d4d97beafb1a272fb52081455
Dhanyashree7 <abdhanyashree@gmail.com>
2026-08-25 10:06:50 +0530

    Updated Q23.py

--- LAB EXP/Q23.py
+++ LAB EXP/Q23.py
@@ -1,1 +1,25 @@
+text = input("Enter text: ")
 
+sentences = text.split(".")
+
+previous_words = set()
+
+score = 0
+
+for sentence in sentences:
+
+    words = set(sentence.lower().split())
+
+    common = previous_words.intersection(words)
+
+    if len(common) > 0:
+        score = score + 1
+
+    previous_words = words
+
+print("Coherence Score:", score)
+
+if score > 0:
+    print("Text is Coherent")
+else:
+    print("Text has Low Coherence")

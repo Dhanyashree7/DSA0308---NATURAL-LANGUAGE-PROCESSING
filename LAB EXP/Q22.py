bc20798147a86efab61fd007aef910b94b3ce0f9
Dhanyashree7 <abdhanyashree@gmail.com>
2026-08-25 10:06:00 +0530

    Updated Q22.py

--- LAB EXP/Q22.py
+++ LAB EXP/Q22.py
@@ -1,1 +1,13 @@
+sentence = input("Enter sentence: ")
 
+words = sentence.split()
+
+previous_noun = ""
+
+for word in words:
+
+    if word.lower() in ["he", "she", "it", "they"]:
+        print(word, "refers to", previous_noun)
+
+    elif word[0].isupper() or word.lower() in ["boy", "girl", "dog", "cat"]:
+        previous_noun = word

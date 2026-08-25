8cd8ab4d48dd486d0679520cc9957401e5efd532
Dhanyashree7 <abdhanyashree@gmail.com>
2026-08-25 10:08:32 +0530

    Updated Q25.py

--- LAB EXP/Q25.py
+++ LAB EXP/Q25.py
@@ -1,1 +1,13 @@
+from openai import OpenAI
 
+client = OpenAI()
+
+prompt = input("Enter prompt: ")
+
+response = client.responses.create(
+    model="gpt-5-mini",
+    input=prompt
+)
+
+print("\nGenerated Text:")
+print(response.output_text)

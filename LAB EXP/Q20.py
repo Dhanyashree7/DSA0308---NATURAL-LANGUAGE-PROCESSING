1405e701a56688f14e3d4759ce42f2ea9d54c4b4
Dhanyashree7 <abdhanyashree@gmail.com>
2026-08-25 10:03:44 +0530

    Updated Q20.py
    Implement a basic TF-IDF scoring system for document retrieval.

--- LAB EXP/Q20.py
+++ LAB EXP/Q20.py
@@ -1,1 +1,29 @@
+import math
 
+documents = [
+    "natural language processing",
+    "machine learning",
+    "natural language learning"
+]
+
+query = input("Enter query: ").lower().split()
+
+for i, document in enumerate(documents):
+
+    words = document.split()
+    score = 0
+
+    for word in query:
+
+        tf = words.count(word) / len(words)
+
+        df = 0
+        for doc in documents:
+            if word in doc.split():
+                df += 1
+
+        if df > 0:
+            idf = math.log(len(documents) / df)
+            score = score + tf * idf
+
+    print("Document", i + 1, "Score:", round(score, 3))
